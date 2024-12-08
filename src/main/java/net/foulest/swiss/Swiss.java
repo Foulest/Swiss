a871b9ecd34ce64ec25580fe3b17dd1b734380db
Foulest <43710301+Foulest@users.noreply.github.com>
2024-12-08 05:01:57 -0800

    Testing teams

--- src/main/java/net/foulest/swiss/Swiss.java
+++ src/main/java/net/foulest/swiss/Swiss.java
@@ -20,7 +20,6 @@ package net.foulest.swiss;
 import lombok.Data;
 import net.foulest.swiss.brackets.ChampionsBracket;
 import net.foulest.swiss.brackets.StandardBracket;
-import net.foulest.swiss.match.Match;
 import net.foulest.swiss.team.Team;
 
 import java.util.ArrayList;
@@ -47,40 +46,40 @@ public final class Swiss {
         Scanner scanner = new Scanner(System.in, "UTF-8").useLocale(Locale.ROOT);
 
         // Create teams with data from HLTV
-        Team g2 = new Team("G2", 1, 2, 1.046);
-        Team natusVincere = new Team("Natus Vincere", 2, 1, 1.098);
-        Team vitality = new Team("Vitality", 3, 3, 1.110);
-        Team spirit = new Team("Spirit", 4, 4, 1.074);
-        Team mouz = new Team("MOUZ", 5, 5, 1.034);
-        Team faze = new Team("FaZe", 6, 6, 1.068);
-        Team heroic = new Team("Heroic", 7, 7, 1.098);
-        Team _3DMAX = new Team("3DMAX", 8, 16, 1.056);
-        Team theMongolZ = new Team("The MongolZ", 9, 9, 1.128);
-        Team liquid = new Team("Liquid", 10, 13, 1.028);
-        Team gamerLegion = new Team("GamerLegion", 11, 20, 1.032);
-        Team furia = new Team("FURIA", 12, 8, 1.044);
-        Team paiN = new Team("paiN", 13, 15, 1.108);
-        Team wildcard = new Team("Wildcard", 14, 22, 1.056);
-        Team big = new Team("BIG", 15, 19, 1.060);
-        Team mibr = new Team("MIBR", 16, 17, 1.064);
+        Team g2 = new Team("G2", 1, 2, 3, 1.056);
+//        Team natusVincere = new Team("Natus Vincere", 2, 1, 1.098); // Eliminated
+        Team vitality = new Team("Vitality", 3, 3, 2, 1.122);
+        Team spirit = new Team("Spirit", 4, 4, 4, 1.076);
+        Team mouz = new Team("MOUZ", 5, 5, 8, 1.042);
+        Team faze = new Team("FaZe", 6, 6, 7, 1.030);
+        Team heroic = new Team("Heroic", 7, 7, 6, 1.094);
+//        Team _3DMAX = new Team("3DMAX", 8, 16, 1.056); // Eliminated
+        Team theMongolZ = new Team("The MongolZ", 9, 9, 1, 1.120);
+        Team liquid = new Team("Liquid", 10, 13, 5, 1.036);
+//        Team gamerLegion = new Team("GamerLegion", 11, 20, 1.032); // Eliminated
+//        Team furia = new Team("FURIA", 12, 8, 1.044); // Eliminated
+//        Team paiN = new Team("paiN", 13, 15, 1.108); // Eliminated
+//        Team wildcard = new Team("Wildcard", 14, 22, 1.056); // Eliminated
+//        Team big = new Team("BIG", 15, 19, 1.060); // Eliminated
+//        Team mibr = new Team("MIBR", 16, 17, 1.064); // Eliminated
 
         // Add teams to the list
         teams.add(g2);
-        teams.add(natusVincere);
+//        teams.add(natusVincere);
         teams.add(vitality);
         teams.add(spirit);
         teams.add(mouz);
         teams.add(faze);
         teams.add(heroic);
-        teams.add(_3DMAX);
+//        teams.add(_3DMAX);
         teams.add(theMongolZ);
         teams.add(liquid);
-        teams.add(gamerLegion);
-        teams.add(furia);
-        teams.add(paiN);
-        teams.add(wildcard);
-        teams.add(big);
-        teams.add(mibr);
+//        teams.add(gamerLegion);
+//        teams.add(furia);
+//        teams.add(paiN);
+//        teams.add(wildcard);
+//        teams.add(big);
+//        teams.add(mibr);
 
         System.out.println("Swiss - CS2 Major Monte Carlo Simulation");
         System.out.println("by Foulest | github.com/Foulest");
@@ -101,12 +100,12 @@ public final class Swiss {
             return;
         }
 
-        // You can also display the winner of a match based on win probability
-        // instead of simulating the entire bracket (these are just examples).
-        if (bracketNumber == 0) {
-            Match.displayWinnerFromProbability(wildcard, gamerLegion, true);
-            return;
-        }
+//        // You can also display the winner of a match based on win probability
+//        // instead of simulating the entire bracket (these are just examples).
+//        if (bracketNumber == 0) {
+//            Match.displayWinnerFromProbability(wildcard, gamerLegion, true);
+//            return;
+//        }
 
         boolean standardBracket = bracketNumber == 1;
 
